29a2eb5e83bc1ed4887a366cf1fbba33198d8476
LuizaLira <82818189+LuizaLira@users.noreply.github.com>
2021-05-17 14:50:39 -0300

    atividade 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-alert('Olá!');
+var contar = document.querySelector('span')
+var original = document.querySelector('#original')
+var modificar = document.querySelector('#modificar')
+var efeito = document.querySelector('#efeito')
+
+original.addEventListener('keyup', () => {
+    contar.innerHTML = original.value.length
+    if(efeito.value == "uppercase") modificar.value = original.value.toUpperCase()
+    else modificar.calue = original.value.toLowerCase()
+  
+  efeito.addEventListener('change', function() {
+    if(efeito.value == "uppercase") modificar.value = original.value.toUpperCase()
+    else modificar.value = original.value.toLowerCase()
+})
